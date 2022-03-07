a3dfc09c34d6a5cdd95bdb405eea3d960de87c6c
Nicolas Hu <courriel.nicolashu@gmail.com>
2022-03-07 15:18:09 +0100

    ménage + ajout fonction Martin

--- advectiondiffusion.cu
+++ advectiondiffusion.cu
@@ -1,4 +1,6 @@
-# include <cstdlib>
+# include <stdio.h>      /* printf, scanf, puts, NULL */
+# include <cstdlib>      /* srand, rand */
+# include <assert.h>     /* assert */
 # include <cmath>
 # include <algorithm>
 # include <iostream>
@@ -10,31 +12,122 @@
 # if not defined(WIN32) && not defined(__USE_POSIX199309)
 #   include <sys/time.h>
 # endif
-# include "Chronometer.hpp"
 
-Chronometer::Chronometer() : m_time(0)
-{}
-// ------------------------------------------------------------------------
-double
-Chronometer::click()
+// For the CUDA runtime routines (prefixed with "cuda_")
+#include <cuda_runtime.h>
+
+#include "walltime.c"
+
+
+void init( int* ndim_tab, int* dim, double* T0, double* x , double* y, double* dx );
+
+void mise_a_jour( int* ndim_tab,   double* T0, double* T1, double* bilan, const double dt );
+void advection( int* ndim_tab,   double* T, double* bilan, double* dx, double* a, int step  );
+void diffusion( int* ndim_tab,   double* T, double* bilan, double* dx, const double mu );
+void condition_limite(int* ndim_tab, double* T, int nfic);
+
+
+__global__ void mise_a_jour( int* ndim_tab,   double* T0, double* T1, double* bilan, const double dt );
+__global__ void advection( int* ndim_tab,   double* T, double* bilan, double* dx, double* a, int step  );
+__global__ void diffusion( int* ndim_tab,   double* T, double* bilan, double* dx, const double mu );
+__global__ void condition_limite(int* ndim_tab, double* T, int nfic);
+
+
+int main( int nargc, char* argv[])
 {
-# ifdef WIN32
-  clock_t chrono;
-  chrono = clock();
-  double t = ((double)chrono)/CLOCKS_PER_SEC;
-# elif defined(CLOCK_MONOTONIC_RAW)
-  struct timespec tp;
-  clock_gettime(CLOCK_MONOTONIC_RAW , &tp);
-  double t = tp.tv_sec+1.E-9*tp.tv_nsec;
-# else
-  struct timeval tv;
-  gettimeofday(&tv,NULL);
-  double t = tv.tv_sec+1.E-6*tv.tv_usec;
-# endif
-  double dt = t - m_time;
-  m_time = t;
-  return dt;
+  char fileName[255];
+  FILE* out;
+
+  int dim[2]; dim[0] = 500; dim[1]=500;
+  int nfic     =  2;
+
+  sprintf(fileName, "Sortie.txt");
+  out = fopen(fileName, "w");
+ 
+
+  //
+  //
+  //Determination la taille des grilles dans les direction X ( Ndim_tab[0]) et Y ( Ndim_tab[1]) avec les cellules fantomes
+  //
+  //
+  int Ndim_tab[2];
+  Ndim_tab[0] = dim[0]+2*nfic; 
+  Ndim_tab[1] = dim[1]+2*nfic;  
+
+
+  double *x,*y, *T1, *T0,  *bilan;
+  x       = new double[Ndim_tab[0]];
+  y       = new double[Ndim_tab[1]];
+  bilan   = new double[Ndim_tab[0]*Ndim_tab[1]]; 
+  T1      = new double[Ndim_tab[0]*Ndim_tab[1]]; 
+  T0      = new double[Ndim_tab[0]*Ndim_tab[1]]; 
+  
+  double dx[2];
+
+  init( Ndim_tab, dim, T0, x, y, dx);
+  fprintf(out, "dim blocX =  %d, dim blocY =  %d, dx= %f, dy= %f \n",Ndim_tab[0], Ndim_tab[1],  dx[0], dx[1] );
+
+  for (int64_t j = 0; j < Ndim_tab[1] ; ++j ){ 
+    for (int64_t i = 0; i < Ndim_tab[0] ; ++i ){ 
+      
+      int    l = j*Ndim_tab[0]+ i;
+      fprintf(out, " Init: %f %f %f   \n", x[i],y[j], T0[l]); 
+    }
+    fprintf(out, " Init: \n"); 
+  }
+
+
+
+  const double dt =0.005;  // pas de temps
+  double U[2];
+  U[0]  =1.;      // vitesse advection
+  U[1]  =1.;
+ 
+  const double mu =0.0005;   // coeff diffusion
+  int Nitmax      =2000;
+  int Stepmax     = 2;
+
+  //Boucle en temps
+  for (int64_t nit = 0; nit < Nitmax ; ++nit )
+  { 
+    //Boucle Runge-Kutta
+    double *Tin;
+    double *Tout;
+    double *Tbilan;
+    for (int64_t step = 0; step < Stepmax ; ++step )
+    {
+      //mise a jour point courant
+      if(step==0) { Tin = T0; Tout= T1; Tbilan= T0;}
+      else        { Tin = T0; Tout= T0; Tbilan= T1;}
+
+      //advection
+      advection(Ndim_tab, Tbilan, bilan,  dx, U , step);
+
+      diffusion(Ndim_tab, Tbilan, bilan,  dx, mu);
+      mise_a_jour(Ndim_tab, Tin, Tout, bilan,  dt);
+
+      //Application Condition limite
+      condition_limite(Ndim_tab, Tout, nfic);     
+
+    }  // Nstepmax
+  }  // Nitmax
+
+  for (int64_t j = 0; j < Ndim_tab[1] ; ++j ){ 
+    for (int64_t i = 0; i < Ndim_tab[0] ; ++i ){ 
+
+      int    l = j*Ndim_tab[0]+ i;
+      fprintf(out, " Final %f %f %f  \n", x[i],y[j], T0[l]); 
+    }
+    fprintf(out, " Final \n"); 
+  }
+
+  fclose(out);
+
+  delete [] T0;  delete [] T1; delete [] bilan; delete [] x;
+
+  return EXIT_SUCCESS;
 }
+
 
 
 
@@ -66,6 +159,9 @@ void init( int* ndim_tab, int* dim, double* T0, double* x , double* y, double* d
     }
   }
 }
+
+
+
 
 ////
 //// mise a jour
@@ -214,97 +310,180 @@ void condition_limite(int* ndim_tab, double* T, int nfic) {
 
 
 
-int main( int nargc, char* argv[])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//On considère les variables suivantes:
+//  . On note ndim_tab[0] : la longueur totale de la grille.
+//  . On note ndim_tab[1] : la largeur totale de la grille.
+//  . dimBlock(blockDim.x, blockDim.y) à définir préalablement de manière à ce que blockDim.x divise ndim_tab[0] et que blockDim.y divise ndim_tab[1].
+//  . dimGrid(ndim_tab[0]/blockDim.x, ndim_tab[1]/blockDim.y)
+
+////
+//// mise a jour
+////
+__global__ void mise_a_jour( int* ndim_tab,   double* T0, double* T1, double* bilan, const double dt )
 {
-  char fileName[255];
-  FILE* out;
+    //On récupère les coordonnées du point à traiter.
+    int i = blockIdx.x * blockDim.x + threadIdx.x;
+    int j = blockIdx.y * blockDim.y + threadIdx.y;
 
-  int dim[2]; dim[0] = 500; dim[1]=500;
-  int nfic     =  2;
+    // Numérotation globale
+    int    l = j*ndim_tab[0]+ i;
 
-  sprintf(fileName, "Sortie.txt");
-  out = fopen(fileName, "w");
- 
-
-  //
-  //
-  //Determination la taille des grilles dans les direction X ( Ndim_tab[0]) et Y ( Ndim_tab[1]) avec les cellules fantomes
-  //
-  //
-  int Ndim_tab[2];
-  Ndim_tab[0] = dim[0]+2*nfic; 
-  Ndim_tab[1] = dim[1]+2*nfic;  
-
-
-  double *x,*y, *T1, *T0,  *bilan;
-  x       = new double[Ndim_tab[0]];
-  y       = new double[Ndim_tab[1]];
-  bilan   = new double[Ndim_tab[0]*Ndim_tab[1]]; 
-  T1      = new double[Ndim_tab[0]*Ndim_tab[1]]; 
-  T0      = new double[Ndim_tab[0]*Ndim_tab[1]]; 
-  
-  double dx[2];
-
-  init( Ndim_tab, dim, T0, x, y, dx);
-  fprintf(out, "dim blocX =  %d, dim blocY =  %d, dx= %f, dy= %f \n",Ndim_tab[0], Ndim_tab[1],  dx[0], dx[1] );
-
-  for (int64_t j = 0; j < Ndim_tab[1] ; ++j ){ 
-    for (int64_t i = 0; i < Ndim_tab[0] ; ++i ){ 
-      
-      int    l = j*Ndim_tab[0]+ i;
-      fprintf(out, " Init: %f %f %f   \n", x[i],y[j], T0[l]); 
+    if(i >= 2 && i < ndim_tab[0] - 2 && j >= 2 && j < ndim_tab[1]){
+        T1[l]    = T0[l] - dt*bilan[l]; 
     }
-    fprintf(out, " Init: \n"); 
-  }
-
-
-
-  const double dt =0.005;  // pas de temps
-  double U[2];
-  U[0]  =1.;      // vitesse advection
-  U[1]  =1.;
- 
-  const double mu =0.0005;   // coeff diffusion
-  int Nitmax      =2000;
-  int Stepmax     = 2;
-
-  //Boucle en temps
-  for (int64_t nit = 0; nit < Nitmax ; ++nit )
-  { 
-    //Boucle Runge-Kutta
-    double *Tin;
-    double *Tout;
-    double *Tbilan;
-    for (int64_t step = 0; step < Stepmax ; ++step )
-    {
-      //mise a jour point courant
-      if(step==0) { Tin = T0; Tout= T1; Tbilan= T0;}
-      else        { Tin = T0; Tout= T0; Tbilan= T1;}
-
-      //advection
-      advection(Ndim_tab, Tbilan, bilan,  dx, U , step);
-
-      diffusion(Ndim_tab, Tbilan, bilan,  dx, mu);
-      mise_a_jour(Ndim_tab, Tin, Tout, bilan,  dt);
-
-      //Application Condition limite
-      condition_limite(Ndim_tab, Tout, nfic);     
-
-    }  // Nstepmax
-  }  // Nitmax
-
-  for (int64_t j = 0; j < Ndim_tab[1] ; ++j ){ 
-    for (int64_t i = 0; i < Ndim_tab[0] ; ++i ){ 
-
-      int    l = j*Ndim_tab[0]+ i;
-      fprintf(out, " Final %f %f %f  \n", x[i],y[j], T0[l]); 
-    }
-    fprintf(out, " Final \n"); 
-  }
-
-  fclose(out);
-
-  delete [] T0;  delete [] T1; delete [] bilan; delete [] x;
-
-  return EXIT_SUCCESS;
 }
+
+
+////
+//// advection
+////
+__global__ void advection( int* ndim_tab,   double* T, double* bilan, double* dx, double* a, int step  )
+{
+
+    double c1 = 7./6.;
+    double c2 = 1./6.;
+    // printf("dx %0.9f %0.9f \n", dx, a*dt ); 
+
+    //On récupère les coordonnées du point à traiter.
+    int i = blockIdx.x * blockDim.x + threadIdx.x;
+    int j = blockIdx.y * blockDim.y + threadIdx.y;
+
+    // 1er sous pas schema Heun
+    if(step==0){
+
+        if(i >= 2 && i < ndim_tab[0] - 2 && j >= 2 && j < ndim_tab[1]){
+            int    l = j*ndim_tab[0]+ i;// (i  , j  )
+            int    l1= l+1;              // (i+1, j  )
+            int    l2= l-1;              // (i-1, j  )
+            int    l3= l-2;              // (i-2, j  )
+            int    l4= l+2;              // (i+2, j  )
+
+            double fm   =(T[l ]+T[l2])*c1 - (T[l1]+T[l3])*c2;
+            double fp   =(T[l1]+T[l ])*c1 - (T[l4]+T[l2])*c2;
+
+            bilan[l] = a[0]*(fp-fm)/(2.*dx[0]); 
+
+            l1= l+ndim_tab[0];     // (i  , j+1)
+            l2= l-ndim_tab[0];     // (i  , j-1)
+            l3= l-2*ndim_tab[0];   // (i  , j+2)
+            l4= l+2*ndim_tab[0];   // (i  , j-2)
+
+            fm   =(T[l ]+T[l2])*c1 - (T[l1]+T[l3])*c2;
+            fp   =(T[l1]+T[l ])*c1 - (T[l4]+T[l2])*c2;
+
+            bilan[l] += a[1]*(fp-fm)/(2.*dx[1]); 
+        }
+    }
+    // 2eme sous pas schema Heun
+    else{
+        if(i >= 2 && i < ndim_tab[0] - 2 && j >= 2 && j < ndim_tab[1]){
+
+            int    l = j*ndim_tab[0]+ i;// (i  , j  )
+            int    l1= l+1;              // (i+1, j  )
+            int    l2= l-1;              // (i-1, j  )
+            int    l3= l-2;              // (i-2, j  )
+            int    l4= l+2;              // (i+2, j  )
+__global__ void diffusion( int* ndim_tab,   double* T, double* bilan, double* dx, const double mu )
+            l1= l+ndim_tab[0];     // (i  , j+1)
+            l2= l-ndim_tab[0];     // (i  , j-1)
+            l3= l-2*ndim_tab[0];   // (i  , j+2)
+            l4= l+2*ndim_tab[0];   // (i  , j-2)
+
+            fm   =(T[l ]+T[l2])*c1 - (T[l1]+T[l3])*c2;
+            fp   =(T[l1]+T[l ])*c1 - (T[l4]+T[l2])*c2;
+
+            bilan[l] += (a[1]*(fp-fm)/(2.*dx[1]))*0.5;
+        }
+    }
+}
+
+__global__ void diffusion( int* ndim_tab,   double* T, double* bilan, double* dx, const double mu )
+{
+    //On récupère les coordonnées du point à traiter.
+    int i = blockIdx.x * blockDim.x + threadIdx.x;
+    int j = blockIdx.y * blockDim.y + threadIdx.y;
+  
+    if(i >= 2 && i < ndim_tab[0] - 2 && j >= 2 && j < ndim_tab[1]){
+
+        int    l = j*ndim_tab[0]+ i;// (i  , j  )
+        int    l1= l+1;              // (i+1, j  )
+        int    l2= l-1;              // (i-1, j  )
+        int    l3= l+ndim_tab[0];   // (i  , j+1)
+        int    l4= l-ndim_tab[0];   // (i  , j-1)
+
+        bilan[l] = bilan[l] - mu*(  (T[l1]+T[l2]-2*T[l])/(dx[0]*dx[0]) +  (T[l3]+T[l4]-2*T[l])/(dx[1]*dx[1]) );
+    }
+}
+
+
+////
+//// condition_limite 
+////
+__global__ void condition_limite(int* ndim_tab, double* T, int nfic) {
+
+    //On récupère les coordonnées du point à traiter.
+    int i = blockIdx.x * blockDim.x + threadIdx.x;
+    int j = blockIdx.y * blockDim.y + threadIdx.y;
+  
+    for (int64_t ific = 0; ific < nfic ; ++ific )
+    {  
+        //periodicite en Jmax et Jmin
+        if(i < ndim_tab[0]){  
+            //Jmin
+            int l0   = ific*ndim_tab[0] +i;
+            int l1   = ndim_tab[0]*(ndim_tab[1]-2*nfic +ific) +i;
+
+            T[l0] = T[l1];
+
+            //Jmax
+            l0   = ndim_tab[0]*(ndim_tab[1]-nfic +ific) +i;
+            l1   = ndim_tab[0]*(nfic +ific) +i;
+
+            T[l0] = T[l1];
+        }
+    }
+
+    for (int64_t ific = 0; ific < nfic ; ++ific )
+    { 
+        //periodicité en Imax et Imin
+        if(j < ndim_tab[1])
+        {  
+            //Imin
+            int l0   = ific +j*ndim_tab[0]; 
+            int l1   = l0 + ndim_tab[0] - 2*nfic;
+
+            T[l0] = T[l1];
+
+            //Imax
+            l0   = ific + (j+1)*ndim_tab[0] - nfic;
+            l1   = l0 - ndim_tab[0] + 2*nfic;
+
+            T[l0] = T[l1];
+        }
+    }
+}
+
+
+
+
+
+
+
+
---
+++ walltime.h
@@ -0,0 +1,6 @@
+#ifndef _WALLTIME_H
+#define _WALLTIME_H
+#include <sys/time.h>
+
+double walltime(double *t0);
+#endif
